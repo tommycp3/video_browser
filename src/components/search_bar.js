081f9ef11029723d5487f7ddfe2ea4456be938e7
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-19 06:19:43 +1000

    fin19-controlled Components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
 
-
 class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: '' };
+        this.state = { term: 'Starting Value' };
     }
 
     render() {
         return (
             <div>
-                <input onChange={event => this.setState({ term: event.target.value })} />
+                <input 
+                value={this.state.term}
+                onChange={event => this.setState({ term: event.target.value })} 
+                />
                 Value of the input: {this.state.term}
             </div>
         );
