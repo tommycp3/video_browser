1fd1880a9e9aea3fe0027d2d245e4c53585d78fb
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-19 05:58:43 +1000

    fin16-handling user events

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,8 +4,9 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input />;
+        return <input onChange={event => console.log(event.target.value)} />;
     }
+
 }
 
 
