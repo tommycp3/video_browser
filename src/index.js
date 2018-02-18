921e2bec554c5ad3e9f3d78a2235438d93b8f2bc
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-19 05:27:31 +1000

    Finished video 11

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
 // create a new componet and make sure it gets converted to html
 
-const App = function () {
+const App = () => {
     return <div>Hi! </div>;
 }
 
 
 // take this component and put it on the page.
 
+ReactDOM.render(<App />, document.querySelector('.container'));
