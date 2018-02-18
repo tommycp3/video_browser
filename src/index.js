5cca0d1c2b81cb2b83508d78a9761c7fa8afaab4
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-19 05:33:04 +1000

    finished video 14 installed youtube module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KYE = 'AIzaSyDinKHsiu9UNKYh9ZHXxQs3f-v4vLqlpyg';
 
 
 // create a new componet and make sure it gets converted to html
