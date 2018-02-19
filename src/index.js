085e8828e71e138f5fb3c69bd7304bf7a5baf719
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-20 06:36:25 +1000

    fin21-youtube_search_response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const API_KYE = 'AIzaSyDinKHsiu9UNKYh9ZHXxQs3f-v4vLqlpyg';
+const API_KEY = 'AIzaSyDinKHsiu9UNKYh9ZHXxQs3f-v4vLqlpyg';
 
+YTSearch({key: API_KEY, term: 'swimming'}, function(data) {
+    console.log(data);
+});
 
 // create a new componet and make sure it gets converted to html
 
