f834d0e977387dff2e520fc3994a0f7d863f8c77
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-20 10:11:21 +1000

    fin22-refactoring_functional_components_to_class_components

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDinKHsiu9UNKYh9ZHXxQs3f-v4vLqlpyg';
 
-YTSearch({key: API_KEY, term: 'swimming'}, function(data) {
-    console.log(data);
-});
 
-// create a new componet and make sure it gets converted to html
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-            <div>Hello</div>
-            
-        </div>
-    );
+class App extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'swimming'}, (videos) => {
+            console.log(videos);
+            this.setState({ videos });
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar />
+                <div>Hello</div>
+                
+            </div>
+        );
+    }
 }
 
-
-// take this component and put it on the page.
 
 ReactDOM.render(<App />, document.querySelector('.container'));
