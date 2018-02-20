b25e1ac2b12fe4c1587a0898905c41336e16514c
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-20 14:16:12 +1000

    fin28 handeling null props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDinKHsiu9UNKYh9ZHXxQs3f-v4vLqlpyg';
 
@@ -24,6 +25,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 
                 <VideoList videos={this.state.videos}/>
                 
