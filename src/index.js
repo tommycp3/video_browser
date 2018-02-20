37f9aaf63514ca37fbf4d6cd83f2311a12d84c79
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2018-02-20 14:35:57 +1000

    fin29 video selection

--- src/index.js
+++ src/index.js
@@ -10,26 +10,34 @@ const API_KEY = 'AIzaSyDinKHsiu9UNKYh9ZHXxQs3f-v4vLqlpyg';
 
 
 class App extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'reality'}, (videos) => {
+        YTSearch({ key: API_KEY, term: 'bigsex' }, (videos) => {
             console.log(videos);
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
-    render () {
+    render() {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                
-                <VideoList videos={this.state.videos}/>
-                
-                
+                <VideoDetail video={this.state.selectedVideo} />
+
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+
+
             </div>
         );
     }
